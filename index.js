3070853a3fd85110c0bf44dc704f5aa188b65c65
Monica Garcia <monicairenegarcia@gmail.com>
2023-12-06 18:19:04 -0700

    completed sending data with fetch lab

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
-// Add your code here
+/*const configurationObject = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/JSON",
+        "Accept": "application/JSON",
+    },
+    body: JSON.stringify({
+        dogName: "Byron",
+        dogBreed: "Poodle",
+    })
+}
+fetch('http://localhost:3000/dogs', configurationObject)
+.then(res => res.json())
+.then(data => console.log(data));*/
+
+
+function submitData(name, email) {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name: "Steve",
+            email: "steve@steve.com",
+        })
+    })
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+        const id = data.id;
+        console.log(id);
+        const divElement = document.createElement("div");
+        divElement.innerHTML = "new element"
+        divElement.setAttribute('id', 'userid');
+        document.body.appendChild(divElement);
+        const userID = document.createElement("p");
+        userID.innerHTML = id;
+        divElement.appendChild(userID);
+    })
+    .catch(error => {
+        alert("Unauthorized Access");
+        const message = error.message;
+        const errorElement = document.createElement("p");
+        errorElement.innerHTML = "Unauthorized Access"
+        document.querySelector("#userid").appendChild(errorElement);
+    })
+    
+    
+}
+submitData("steve", "steve@steve.com");
